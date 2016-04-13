3382644dd87f23c570b63c7eaf9eafb1a36f22d9
Gizeta <0w0@gizeta.tk>
2016-04-13 19:52:37 +0900

    init immediately

--- task43/album.js
+++ task43/album.js
@@ -1,37 +1,56 @@
 var Album = (function() {
-  /* You are squares! */
-  function fix3(images, width, height) {
-    var val1 = height / 2, val2 = width - val1;
-    images[0].style.width = val2 + "px";
-    images[1].style.width = val1 + "px";
-    images[1].style.left = val2 + "px";
-    images[2].style.width = val1 + "px";
-    images[2].style.left = val2 + "px";
-  }
-  function fix5(images, width, height) {
-    var val1 = width / 3, val2 = height - val1;
-    images[1].style.height = val1 + "px";
-    images[4].style.height = val2 + "px";
-    images[4].style.top = val1 + "px";
+  function fixWrongStyle(node, width, height) {
+    var childCount = node.children.length;
+    if (childCount == 3) {
+      fix3(node.children);
+    }
+    else if (childCount == 5) {
+      fix5(node.children);
+    }
+
+    /* You are squares! */
+    function fix3(images) {
+      var val1 = height / 2, val2 = width - val1;
+      images[0].style.width = val2 + "px";
+      images[1].style.width = val1 + "px";
+      images[1].style.left = val2 + "px";
+      images[2].style.width = val1 + "px";
+      images[2].style.left = val2 + "px";
+    }
+    function fix5(images) {
+      var val1 = width / 3, val2 = height - val1;
+      images[1].style.height = val1 + "px";
+      images[4].style.height = val2 + "px";
+      images[4].style.top = val1 + "px";
+    }
   }
 
   return {
-    init: function(id, w, h) {
-      var container = document.getElementById(id);
-      container.className += " album";
-      container.style.width = w + "px";
-      container.style.height = h + "px";
+    init: function() {
+      var nodes = document.querySelectorAll('.album');
+      [].forEach.call(nodes, function(node) {
+        width = node.dataset.width || node.clientWidth;
+        height = node.dataset.height || node.clientHeight;
 
-      width = w;
-      height = h;
+        node.style.width = width + "px";
+        node.style.height = height + "px";
+        fixWrongStyle(node, width, height);
+      });
+    },
+    setUp: function(id, options) {
+      var node = document.getElementById(id);
+      node.className += " album";
 
-      var childCount = container.children.length;
-      if (childCount == 3) {
-        fix3(container.children, w, h);
-      }
-      else if (childCount == 5) {
-        fix5(container.children, w, h);
-      }
+      options = options || {};
+      width = options.width || node.clientWidth;
+      height = options.height || node.clientHeight;
+
+      node.style.width = width + "px";
+      node.style.height = height + "px";
+
+      fixWrongStyle(node, width, height);
     }
   }
 })();
+
+Album.init();
